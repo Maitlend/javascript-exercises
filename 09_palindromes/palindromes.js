0e6a4bd15d9fb5c00b47f738906fe5dea667655b
Maitlend <Maitlend.merrill@gmail.com>
2022-11-01 16:13:36 -0400

    Add solution for exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+  let charArr = Array.from(word.toLowerCase());
+  charArr = charArr.filter(char => char.charCodeAt(0) >= 97 && char.charCodeAt(0) <= 122);
+  for(let i = 0; i < charArr.length/2 ; i++){
+    if(charArr[i] != charArr[charArr.length-1-i]){
+      return false
+    }
+  }
+  return true;
 };
 
 // Do not edit below this line
